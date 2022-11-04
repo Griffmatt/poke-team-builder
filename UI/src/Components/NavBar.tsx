035fae6840529fa6c9206c0b6f0233aa29341b04
Griffin <griffinmmatthews@gmail.com>
2022-11-03 20:49:30 -0400

    added navlinks

--- UI/src/Components/NavBar.tsx
+++ UI/src/Components/NavBar.tsx
@@ -4,11 +4,31 @@ import { useThemeContext } from '../Context/themeContext'
 export default function NavBar() {
   const { darkMode, handleDarkMode } = useThemeContext()
     const user = false
+
+    const navoptions = [
+        {
+            value: "Home",
+            link: "/"
+        },
+        {
+            value: "Pokemon",
+            link: "/pokemon"
+        },
+        {
+            value: "Teams",
+            link: "/teams"
+        },
+    ]
+
   return (
-    <div className="flex justify-around p-3 max-w-[30rem] mx-auto">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/pokemon">Pokemon</NavLink>
-      <NavLink to="/teams">Teams</NavLink>
+    <div className="flex justify-around p-3 max-w-[40rem] mx-auto">
+      {navoptions.map(option =>{
+        return(
+            <NavLink to={option.link}  className={({ isActive }) =>
+            isActive ? "border-b-2 border-black dark:border-white" : undefined
+          }>{option.value}</NavLink>
+        )
+      })}
       <button onClick={handleDarkMode}>
         <h2>{darkMode ? 'Light' : 'Dark'}</h2>
       </button>
