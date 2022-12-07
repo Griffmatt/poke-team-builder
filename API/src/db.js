2badb2b8030a4be389b1fbfa8641af6fd5a4f797
Griffin <griffinmmatthews@gmail.com>
2022-12-06 21:28:48 -0500

    redploy

--- API/src/db.js
+++ API/src/db.js
@@ -1,11 +1,19 @@
 const { Pool } = require('pg')
 
-const pool = new Pool({
+const devPool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'griff-team-builder',
   password: 'postgres',
   port: 5432
+})
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'containers-us-west-98.railway.app',
+  database: 'railway',
+  password: 'SBCZHr6JnC0DuU76ttra',
+  port: 6302
 })
 
 module.exports = pool
