397f9e8fd4fa76765ea78ccb8aca4eec10940ad0
Griffin <griffinmmatthews@gmail.com>
2022-12-28 14:05:37 -0500

    add team page to allow user to share and view other teams

--- UI/src/Pages/Team/Team.tsx
+++ UI/src/Pages/Team/Team.tsx
@@ -1,3 +1,43 @@
+import { useQueries } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
+import PokemonCard from '../../Components/PokemonCard'
+import LoadingSpinner from '../../Components/UI/LoadingSpinner'
+import fetchSingleTeam from '../../Utils/fetch/Database/fetchSingleTeam'
+import fetchUser from '../../Utils/fetch/Database/fetchUser'
+
 export default function Team() {
-  return <div>Team</div>
+  const { userId, teamId } = useParams()
+
+
+  const results = useQueries({
+    queries: [
+      { queryKey: ['user', Number(userId)], queryFn: () => fetchUser(Number(userId))},
+      { queryKey: ['team', Number(teamId)], queryFn: () => fetchSingleTeam(Number(teamId))}
+    ]
+  })
+
+  const isLoading = results[0].isLoading && results[1].isLoading
+  
+  const teamsUser = results[0].data
+  const team = results[1].data
+
+  if(isLoading) return <LoadingSpinner/>
+
+  return (
+    <div>
+      <h2>{teamsUser?.name}'s Team</h2>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+        {team?.pokemon.map((pokemon, index) => {
+          return (
+            <div key={index} className="p-2">
+              <PokemonCard
+                pokemonName={pokemon.pokemon_name}
+                createdPokemonName={pokemon.name}
+              />
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
---
+++ UI/src/Utils/fetch/Database/fetchSingleTeam.ts
@@ -0,0 +1,10 @@
+import axios from 'axios'
+import { UsersCreatedTeam } from '../../../Typescript/interfaces'
+
+const url = import.meta.env.VITE_BASE_URL
+
+export default async function fetchUsersTeams(teamId?: number ) {
+  if(teamId == null) return null
+  const response = await axios.get<UsersCreatedTeam>(`${url}/teams/team/${teamId}`)
+  return response.data
+}
