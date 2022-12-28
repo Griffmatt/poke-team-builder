26e9b99c08bd109ef2274f4db518dec33dfc3e81
Griffin Matthews <68610081+Griffmatt@users.noreply.github.com>
2022-12-28 14:30:58 -0500

    Update db.js

--- API/src/db.js
+++ API/src/db.js
@@ -8,12 +8,5 @@ const devPool = new Pool({
   port: 5432
 })
 
-/* const pool = new Pool({
-  user: 'postgres',
-  host: 'containers-us-west-98.railway.app',
-  database: 'railway',
-  password: 'SBCZHr6JnC0DuU76ttra',
-  port: 6302
-}) */
 
 module.exports = devPool
