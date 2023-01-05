4217305ac8e704ac288cc5c24026870176d54f5f
Griffin <griffinmmatthews@gmail.com>
2023-01-04 19:08:11 -0500

    change padding for loading spinner to center

--- UI/src/Components/UI/LoadingSpinner.tsx
+++ UI/src/Components/UI/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 export default function LoadingSpinner() {
     return (
-      <div className="flex justify-center items-center h-full">
+      <div className="flex justify-center items-center pt-[25vh]">
         <div className="w-16 h-16 border-4 border-primary border-solid rounded-full animate-spin border-t-dark/5"></div>
       </div>
     )
