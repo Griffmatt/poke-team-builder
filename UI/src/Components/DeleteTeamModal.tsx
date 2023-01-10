4e6f81850c6cc5fdf16ff141599ca24f36efa06b
Griffin <griffinmmatthews@gmail.com>
2023-01-10 18:31:01 -0500

    update ui for confirm delete team modal

--- UI/src/Components/DeleteTeamModal.tsx
+++ UI/src/Components/DeleteTeamModal.tsx
@@ -15,18 +15,19 @@ export default function DeleteTeamModal({ name, teamId, showDeleteModal, setShow
     
     return (
       <div
-        className={`fixed top-0 left-0  w-screen h-screen place-items-center bg-dark/50 dark:bg-light/5 z-50 ${
+        className={`fixed top-0 left-0  w-screen h-screen place-items-center bg-dark/50 z-50 ${
           showDeleteModal ? 'grid grid-rows-3' : 'hidden'
         }`}
         onClick={() => setShowDeleteModal(false)}
       >
         <div
-          className="bg-light dark:bg-dark rounded-xl p-4 grid gap-4"
+          className="bg-light dark:bg-dark rounded-xl p-6 grid gap-4 w-[90vw] max-w-[24rem]"
           onClick={(event) => event?.stopPropagation()}
         >
-          <h2>
-            Are you sure you want <br /> to delete {name}?
-          </h2>
+          <h2>Confirm Delete</h2>
+          <h3 className="text-center">
+            Are you sure you want to delete {name}?
+          </h3>
           <div className="flex justify-around">
             <button
               className="rounded py-1 px-2 "
