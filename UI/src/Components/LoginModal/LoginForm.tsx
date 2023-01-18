41fa4425df440d961ffb626a39cc732248ccb944
Griffin <griffinmmatthews@gmail.com>
2023-01-18 18:48:24 -0500

    update outline of inputs when there is an error

--- UI/src/Components/LoginModal/LoginForm.tsx
+++ UI/src/Components/LoginModal/LoginForm.tsx
@@ -25,6 +25,7 @@ export default function LoginForm({ error, setError }: Props) {
     })
     if (!user) {
       setError(true)
+      console.log(user)
       return null
     }
     setError(false)
